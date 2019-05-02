0e8b42cc44557fe48fb29abecc2b76e6c20f08b3
RebeccaZuo <rebecca_zuo@brown.edu>
2019-05-02 10:53:07 -0400

    updating before pulling

--- edtech/processing.py
+++ edtech/processing.py
@@ -3,19 +3,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sqlite3 import Error
 
+database = "/Users/rebeccazuo/Desktop/DataScienceFinal/edtech/all_data.db"
+
+columns = ('idn', 'dist_size', 'urb', 'region', 'totalComputers', 'computersForInstruction', 'integration', 'training')
+
+def create_connection(db_file):
+	try:
+		conn = sqlite3.connect(db_file)
+		return conn
+	except:
+		print("ERROR: Could not create connection.")
+
+conn = create_connection(database)
+cur = conn.cursor()
+
+def create_table(conn, sql_text):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+        c.close()
+    except Error as e:
+        print(e)
+
+create_table_sql = """ CREATE TABLE IF NOT EXISTS edTech (
+                    dist_size integer,
+                    urb integer,
+                    region integer,
+                    totalComputers integer NOT NULL ,
+                    computersForInstruction integer ,
+                    integration integer,
+                    training integer
+                ); """
+create_table(conn,create_table_sql)
+
+insert_sql  = """ INSERT INTO edTech(
+    dist_size ,
+    urb ,
+    region ,
+    totalComputers  ,
+    computersForInstruction  ,
+    integration ,
+    training )
+
+    VALUES(?,?,?,?,?,?,?)
+    """
+
+
+connection = create_connection(database)
+
+if connection is not None:
+    create_table(connection, database)
+else:
+    print("Not connecting")
+
 
 read_data = []
+processed_data = []
 with open('frss92.dat', 'r') as datafile:
     for line in datafile:
         read_data.append(line)
-
-# each line has 1442 chars, 1527 lines total
-# quality measures? sum of 10-77
-# barrier measures? 80-93 (providing), 96-111 (student participation)
-# resource limitations for adding + phasing out? 118-123 (adding), 138-143 (removing)
-# all of the above aren't zero-indexed (based on key provided w dataset)
-
-processed_data = []
 
 
 def get_score(subarr):
@@ -24,6 +70,12 @@ def get_score(subarr):
         if (elt not in [" ", "-", "8"]):
             score += int(elt)
     return score
+
+def add_data(cur, arr, sql_statement):
+    cur.execute(sql_statement, arr)
+    conn.commit()
+    return cur.lastrowid
+
 
 for line in read_data:
 
@@ -37,58 +89,55 @@ for line in read_data:
         region = line[9] # 1 = northeast, 2 = southeast, 3 = central, 4 = west
 
         # response vars
-        totalComputers = get_score(line[16-19])
-        computersForInstruction = get_score(line[24-27])
+        totalComputers = get_score(line[16:27]) #
+        computersForInstruction = get_score(line[24:27]) #
         integration = get_score(line[135]) # on a range of 1-4 if district staff help technology integration
-        training = get_score(line[154])
+        training = get_score(line[154]) # on a range of 1-4 are teachers sufficiently trained to integrate technology
+
+		#a combined column for everything
+        # if (urban == 1 and region == 1):
+		#  	combined = 1
+	    # if(urban == 1 and region == 2):
+		#     combined = 2
+	    # if(urban == 1 and region == 3):
+		#     combined = 3
+	    # if(urban == 1 and region == 4):
+		#     combined = 4
+	    # if(urban == 2 and region == 1):
+		#     combined = 5
+	    # if(urban == 2 and region == 2):
+		#     combined = 6
+	    # if(urban == 2 and region == 3):
+		#     combined = 7
+	    # if(urban == 2 and region == 4):
+		#     combined = 8
+	    # if(urban == 3 and region == 1):
+		#     combined = 9
+	    # if(urban == 3 and region == 2):
+		#     combined = 10
+	    # if(urban == 3 and region == 3):
+		#     combined = 11
+	    # if(urban == 3 and region == 4):
+		#     combined = 12
+	    # if(urban == 4 and region == 1):
+		#     combined = 13
+	    # if(urban == 4 and region == 2):
+		#     combined = 14
+	    # if(urban == 4 and region == 3):
+		#     combined = 15
+	    # if(urban == 4 and region == 4):
+		#     combined = 16
 
         # add to processed_data
-        dist_data = [idn, dist_size, urb, region, totalComputers, computersForInstruction, integration, training]
+        dist_data = [dist_size, urb, region, totalComputers, computersForInstruction, integration, training]
         processed_data.append(dist_data)
+        x = add_data(cur, dist_data, insert_sql)
 
 
+
+
+conn.close()
 # no nulls in data
 
 print(len(processed_data)) # = 916
-print(len(processed_data[0])) # = 8
-
-database = "all_data.db"
-
-columns = ('idn', 'dist_size', 'urb', 'region', 'totalComputers', 'computersForInstruction', 'integration', 'training')
-
-def create_connection(db_file):
-    try :
-        # connection = sqlite3.connect("edTech.db")
-        connection = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
-    # finally:
-    #     connection.close()
-    return connection
-
-def create_table(conn, sql_text):
-    try:
-        c = conn.cursor()
-        c.execute(districtTable)
-        c.close()
-    except Error as e:
-        print(e)
-
-districtTable  = """ CREATE TABLE IF NOT EXISTS edtech(
-    dist_size integer,
-    urb integer,
-    region integer,
-    totalComputers integer NOT NULL ,
-    computersForInstruction integer ,
-    integration integer,
-    training integer,
-    idn integer PRIMARY KEY
-);"""
-
-connection = create_connection("database.db")
-
-if connection is not None:
-    create_table(connection, "database.db")
-else:
-    print("Not connecting")
-
+print(len(processed_data[0])) # = 7
