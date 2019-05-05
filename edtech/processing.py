cc7b6c00a9a6a81434be83ef3f7c47969b50b4b3
RebeccaZuo <rebecca_zuo@brown.edu>
2019-05-04 20:46:35 -0400

    changes

--- edtech/processing.py
+++ edtech/processing.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sqlite3 import Error
 
-database = "/Users/rebeccazuo/Desktop/DataScienceFinal/edtech/all_data.db"
+database = "/Users/rebeccazuo/Desktop/cs1951a-final/all_data.db"
 
-columns = ('idn', 'dist_size', 'urb', 'region', 'totalComputers', 'computersForInstruction', 'integration', 'training')
+columns = ('id', 'dist_size', 'urb', 'region', 'totalComputers', 'computersForInstruction', 'integration', 'training')
 
 def create_connection(db_file):
 	try:
@@ -17,6 +17,7 @@ def create_connection(db_file):
 conn = create_connection(database)
 cur = conn.cursor()
 
+
 def create_table(conn, sql_text):
     try:
         c = conn.cursor()
@@ -25,7 +26,10 @@ def create_table(conn, sql_text):
     except Error as e:
         print(e)
 
+
+
 create_table_sql = """ CREATE TABLE IF NOT EXISTS edTech (
+					combined integer,
                     dist_size integer,
                     urb integer,
                     region integer,
@@ -34,9 +38,18 @@ create_table_sql = """ CREATE TABLE IF NOT EXISTS edTech (
                     integration integer,
                     training integer
                 ); """
+
+
+# add_column = "ALTER TABLE edTech ADD id integer;"
+
+sql = "DROP TABLE edTech"
+cur.execute(sql)
+
 create_table(conn,create_table_sql)
 
+
 insert_sql  = """ INSERT INTO edTech(
+	combined,
     dist_size ,
     urb ,
     region ,
@@ -45,8 +58,9 @@ insert_sql  = """ INSERT INTO edTech(
     integration ,
     training )
 
-    VALUES(?,?,?,?,?,?,?)
+    VALUES(?,?,?,?,?,?,?,?)
     """
+
 
 
 connection = create_connection(database)
@@ -85,51 +99,48 @@ for line in read_data:
 
         # joinable attributes
         dist_size = line[7] # 1 = less than 2500k, 2 = 2500k < 9999k, 3 = 10000k+
-        urb = line[8] # 1 = city, 2 = suburban, 3 = town, 4 = rural
-        region = line[9] # 1 = northeast, 2 = southeast, 3 = central, 4 = west
+        urb = int(line[8]) # 1 = city, 2 = suburban, 3 = town, 4 = rural
+        region = int(line[9]) # 1 = northeast, 2 = southeast, 3 = central, 4 = west
 
         # response vars
         totalComputers = get_score(line[16:27]) #
         computersForInstruction = get_score(line[24:27]) #
         integration = get_score(line[135]) # on a range of 1-4 if district staff help technology integration
         training = get_score(line[154]) # on a range of 1-4 are teachers sufficiently trained to integrate technology
-
-		#a combined column for everything
-        # if (urban == 1 and region == 1):
-		#  	combined = 1
-	    # if(urban == 1 and region == 2):
-		#     combined = 2
-	    # if(urban == 1 and region == 3):
-		#     combined = 3
-	    # if(urban == 1 and region == 4):
-		#     combined = 4
-	    # if(urban == 2 and region == 1):
-		#     combined = 5
-	    # if(urban == 2 and region == 2):
-		#     combined = 6
-	    # if(urban == 2 and region == 3):
-		#     combined = 7
-	    # if(urban == 2 and region == 4):
-		#     combined = 8
-	    # if(urban == 3 and region == 1):
-		#     combined = 9
-	    # if(urban == 3 and region == 2):
-		#     combined = 10
-	    # if(urban == 3 and region == 3):
-		#     combined = 11
-	    # if(urban == 3 and region == 4):
-		#     combined = 12
-	    # if(urban == 4 and region == 1):
-		#     combined = 13
-	    # if(urban == 4 and region == 2):
-		#     combined = 14
-	    # if(urban == 4 and region == 3):
-		#     combined = 15
-	    # if(urban == 4 and region == 4):
-		#     combined = 16
-
+        if (urb == 1 and region == 1):
+	        combined = 1
+        elif(urb == 1 and region == 2):
+	        combined = 2
+        elif(urb== 1 and region == 3):
+	        combined = 3
+        elif(urb== 1 and region == 4):
+	        combined = 4
+        elif(urb == 2 and region == 1):
+	        combined = 5
+        elif(urb == 2 and region == 2):
+	        combined = 6
+        elif(urb == 2 and region == 3):
+	        combined = 7
+        elif(urb == 2 and region == 4):
+	        combined = 8
+        elif(urb == 3 and region == 1):
+	        combined = 9
+        elif(urb == 3 and region == 2):
+	        combined = 10
+        elif(urb == 3 and region == 3):
+	        combined = 11
+        elif(urb == 3 and region == 4):
+	        combined = 12
+        elif(urb == 4 and region == 1):
+	        combined = 13
+        elif(urb == 4 and region == 2):
+	        combined = 14
+        elif(urb == 4 and region == 3):
+	        combined = 15
+        elif(urb == 4 and region == 4):
+	        combined = 16
         # add to processed_data
-        dist_data = [dist_size, urb, region, totalComputers, computersForInstruction, integration, training]
+        dist_data = [combined, dist_size, urb, region, totalComputers, computersForInstruction, integration, training]
         processed_data.append(dist_data)
         x = add_data(cur, dist_data, insert_sql)
 
@@ -139,5 +150,5 @@ for line in read_data:
 conn.close()
 # no nulls in data
 
-print(len(processed_data)) # = 916
-print(len(processed_data[0])) # = 7
+#print(len(processed_data)) # = 916
+#print(len(processed_data[0])) # = 7
